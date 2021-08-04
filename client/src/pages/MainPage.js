4292bd37b5a9235180c4eb7ea18e6c29c176ee56
ymink716 <ymink716@gmail.com>
2021-08-04 16:49:51 +0900

    fix: main page useEffect

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Layout, PageHeader } from 'antd';
+import { PageHeader } from 'antd';
 import axios from 'axios';
 import { Row } from 'antd';
 import UnitCard from '../components/UnitCard';
 import qs from 'qs';
 
-const { Content } = Layout;
 
 function MainPage(props) {
     const [units, setUnits] = useState([]);
@@ -16,17 +15,23 @@ function MainPage(props) {
     });
     const searchText = query.search;
 
-    useEffect(async () => {
-        try {
-            let response;
-            if (searchText) response = await axios.get(`/api/unit/search/${searchText}`);
-            else response = await axios.get('/api/unit');
-            
-            if (response.data.success) setUnits(response.data.units);
-            else alert(response.data.message);
-        } catch (error) {
-            console.error(error);
-            alert('데이터를 불러오는데 실패했습니다.');
+    useEffect(() => {
+        if (searchText) {
+            axios.get(`/api/unit/search/${searchText}`)
+            .then(response => {
+                setUnits(response.data.units);
+            }).catch(error => {
+                console.error(error);
+                alert('데이터를 불러오는데 실패했습니다.');
+            })
+        } else {
+            axios.get('/api/unit')
+            .then(response => {
+                setUnits(response.data.units);
+            }).catch(error => {
+                console.error(error);
+                alert('데이터를 불러오는데 실패했습니다.');
+            });
         }
     }, []);
 
